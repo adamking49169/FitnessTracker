cd0a9f70c99382a4172bfd5924b8a45f1eea8715
adamking49169 <adamking49169@gmail.com>
2025-06-30 19:41:33 +0100

    Adding food api

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace FitnessTracker.Controllers
             return View();
         }
 
+        public IActionResult FoodSearch()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
