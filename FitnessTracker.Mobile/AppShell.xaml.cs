ad0256a03aa7b4d02506bd1c437d8cdbbd4b56f9
adamking49169 <adamking49169@gmail.com>
2025-07-20 23:50:41 +0000

    Remove font binaries

---
+++ FitnessTracker.Mobile/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace FitnessTracker.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+        Routing.RegisterRoute("login", typeof(Views.LoginPage));
+        Routing.RegisterRoute("workouts", typeof(Views.WorkoutsPage));
+    }
+}
